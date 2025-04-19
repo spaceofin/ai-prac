2abbf905e50b01546c19073117db21952686529a
spaceofin <80182521+spaceofin@users.noreply.github.com>
2025-04-19 20:25:42 +0900

    [llama-index-practice]chore: install llamaindex, @llamaindex/ollama and set up next.config.ts

--- llama-index-practice/next.config.ts
+++ llama-index-practice/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import withLlamaIndex from "llamaindex/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withLlamaIndex(nextConfig);
